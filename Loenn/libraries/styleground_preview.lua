ec2191fd3921781c353c6d3a13064bf3365060a8
microlith57 <microlith57@gmail.com>
2023-01-07 22:34:13 +1300

    use clamp instead of min/max

--- Loenn/libraries/styleground_preview.lua
+++ Loenn/libraries/styleground_preview.lua
@@ -51,8 +51,8 @@ function preview.previewPos(state)
   if preview.snap_to_room and state then
     local room = state.getSelectedRoom()
     if room and room.x and room.y and room.width and room.height then
-      pos_x = math.min(math.max(room.x, pos_x), room.x + room.width - canvasWidth)
-      pos_y = math.min(math.max(room.y, pos_y), room.y + room.height - canvasHeight - 4)
+      pos_x = utils.clamp(room.x, pos_x, room.x + room.width - canvasWidth)
+      pos_y = utils.clamp(room.y, pos_y, room.y + room.height - canvasHeight - 4)
     end
   end
 
