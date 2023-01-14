d8aee93c48c5f08043401b7048ba24ff43156471
microlith57 <microlith57@gmail.com>
2023-01-14 17:01:30 +1300

    improve menu; warn if colorgrades are enabled

--- Loenn/libraries/patch_menu.lua
+++ Loenn/libraries/patch_menu.lua
@@ -11,6 +11,17 @@ local colorgradePreview = require("mods").requireFromPlugin("libraries.preview.c
 
 ---
 
+local function submenu(menu, lang)
+  local item = $(menu):find(item -> item[1] == lang)
+  if not item then
+    item = {}
+    table.insert(menu, item)
+  end
+  item[1] = lang
+  item[2] = {}
+  return item[2]
+end
+
 local function checkbox(menu, lang, toggle, active)
   local item = $(menu):find(item -> item[1] == lang)
   if not item then
@@ -30,25 +41,33 @@ end
 ]]
 
 local viewMenu = $(menubar):find(menu -> menu[1] == "view")[2]
+local stylegroundMenu = submenu(viewMenu, "anotherloennplugin_preview_styleground")
 
-checkbox(viewMenu, "anotherloennplugin_styleground_preview_bg",
+checkbox(stylegroundMenu, "anotherloennplugin_preview_styleground_bg",
          stylegroundPreview.toggle_bg,
          function() return stylegroundPreview.bg_enabled end)
 
-checkbox(viewMenu, "anotherloennplugin_styleground_preview_fg",
+checkbox(stylegroundMenu, "anotherloennplugin_preview_styleground_fg",
          stylegroundPreview.toggle_fg,
          function() return stylegroundPreview.fg_enabled end)
 
-checkbox(viewMenu, "anotherloennplugin_styleground_preview_snap",
+checkbox(stylegroundMenu, "anotherloennplugin_preview_styleground_snap",
          stylegroundPreview.toggle_snap,
          function() return stylegroundPreview.snap_to_room end)
 
-checkbox(viewMenu, "anotherloennplugin_styleground_preview_anim",
+checkbox(stylegroundMenu, "anotherloennplugin_preview_styleground_anim",
          stylegroundPreview.toggle_anim,
          function() return stylegroundPreview.anim_start ~= nil end)
 
 if colorgradePreview then
-  checkbox(viewMenu, "anotherloennplugin_colorgrade_preview",
+  local logging = require("logging")
+  logging.warning("[AnotherLoennPlugin] ---")
+  logging.warning("[AnotherLoennPlugin] Colorgrade preview enabled!")
+  logging.warning("[AnotherLoennPlugin] Any problems you experience should be reported to microlith57#4004, even if they appear to be unrelated.")
+  logging.warning("[AnotherLoennPlugin] Before reporting anything, first try moving the AnotherLoennPluginColorgrading plugin out of your Mods folder!")
+  logging.warning("[AnotherLoennPlugin] ---")
+
+  checkbox(viewMenu, "anotherloennplugin_preview_colorgrade",
           colorgradePreview.toggle,
           function() return colorgradePreview.enabled end)
 end
