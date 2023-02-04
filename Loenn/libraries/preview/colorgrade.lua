48828ed6a7fd4666913be1f913b61c7a5c1f68ef
microlith57 <microlith57@gmail.com>
2023-02-04 16:35:19 +1300

    experiment: switch to using a canvas

--- Loenn/libraries/preview/colorgrade.lua
+++ Loenn/libraries/preview/colorgrade.lua
@@ -6,66 +6,53 @@ end
 
 local fileLocations = require("file_locations")
 local utils = require("utils")
+local uiu = require("ui.utils")
 local modHandler = require("mods")
+local viewportHandler = require("viewport_handler")
 
 ---
 
-local preview = {}
-preview.enabled = false
+local col_preview = {}
+col_preview.enabled = false
 
-function preview.toggle()
-  if preview.enabled then
-    preview.enabled = false
-    preview.currentColorgrade = nil
+function col_preview.toggle()
+  if col_preview.enabled then
+    col_preview.enabled = false
+    col_preview.unload()
   else
-    preview.enabled = true
+    col_preview.enabled = true
   end
 end
 
-local importantBit = [[
+col_preview.shaderSource = [[
   uniform VolumeImage colorgrade;
 
   vec4 grade(vec4 color) {
     return Texel(colorgrade, color.rgb) * color.a;
   }
-]]
 
-preview.pixShaderSource = importantBit .. [[
   vec4 effect(vec4 vcolor, Image tex, vec2 texcoord, vec2 pixcoord) {
     return grade(Texel(tex, texcoord) * vcolor);
   }
 ]]
 
-preview.arrShaderSource = importantBit .. [[
-  uniform ArrayImage MainTex;
+col_preview.shader = nil
+col_preview.currentColorgrade = nil
+col_preview.textureValid = nil
 
-  void effect() {
-    love_PixelColor = grade(Texel(MainTex, VaryingTexCoord.xyz) * VaryingColor);
-  }
-]]
-
-preview.pixShader = nil
-preview.arrShader = nil
-preview.currentColorgrade = nil
-preview.textureValid = nil
-
-function preview.setupShaders(state)
-  if not preview.pixShader then
-    preview.pixShader = love.graphics.newShader(preview.pixShaderSource)
-    preview.currentColorgrade = nil
-  end
-  if not preview.arrShader then
-    preview.arrShader = love.graphics.newShader(preview.arrShaderSource)
-    preview.currentColorgrade = nil
+function col_preview.setupShader(state)
+  if not col_preview.shader then
+    col_preview.shader = love.graphics.newShader(col_preview.shaderSource)
+    col_preview.currentColorgrade = nil
   end
 
   if not state or not state.side or not state.side.meta then return end
 
   local colorgrade = state.side.meta.ColorGrade
 
-  if preview.enabled and preview.currentColorgrade ~= colorgrade then
-    preview.textureValid = false
-    preview.currentColorgrade = colorgrade
+  if col_preview.enabled and col_preview.currentColorgrade ~= colorgrade then
+    col_preview.textureValid = false
+    col_preview.currentColorgrade = colorgrade
     local path = utils.joinpath(fileLocations.getCelesteDir(), "Content", "Graphics", "ColorGrading", colorgrade) .. ".png"
     local fileData, err = love.filesystem.newFileData(utils.readAll(path, "rb"), colorgrade .. ".png")
     if not fileData then
@@ -89,70 +76,79 @@ function preview.setupShaders(state)
             layer:setPixel(x, y, r, g, b, a)
           end
         end
-
       end
 
       local image = love.graphics.newVolumeImage(layers)
       image:setFilter("linear", "linear")
 
-      preview.pixShader:send("colorgrade", image)
-      preview.arrShader:send("colorgrade", image)
-      preview.textureValid = true
+      col_preview.shader:send("colorgrade", image)
+      col_preview.textureValid = true
     end
   end
 end
 
 ---
 
-preview._orig_draw = love.graphics.draw
-preview._mod_draw = function(drawable, a, b, c, d, e, f, g, h, i, j)
-  if (drawable:typeOf("Image") and drawable:getTextureType() == "array")
-    or (drawable:typeOf("SpriteBatch") and drawable:getTexture():getTextureType() == "array")
-    or (drawable:typeOf("Mesh") and drawable:getTexture():getTextureType() == "array") then
-    if love.graphics.getShader() ~= preview.arrShader then
-      love.graphics.setShader(preview.arrShader)
-    end
-  else
-    if love.graphics.getShader() ~= preview.pixShader then
-      love.graphics.setShader(preview.pixShader)
-    end
+col_preview.canvas = nil
+local prev_canvas
+local resize_timer = 0
+
+function col_preview.begin_preview(state)
+  if resize_timer > 0 then
+    resize_timer -= 1
   end
-  preview._orig_draw(drawable, a, b, c, d, e, f, g, h, i, j)
-end
 
--- hope no-one uses drawInstanced!
-
-preview._orig_drawLayer = love.graphics.drawLayer
-preview._mod_drawLayer = function(texture, layerindex, a, b, c, d, e, f, g, h, i, j)
-  love.graphics.setShader(preview.arrShader)
-  preview._orig_drawLayer(texture, layerindex, a, b, c, d, e, f, g, h, i, j)
-end
-
-preview.orig_shader = nil
-
-function preview.begin_preview(state)
-  preview.setupShaders(state)
-  if not preview.enabled or not preview.textureValid then
+  col_preview.setupShader(state)
+  if not col_preview.enabled or not col_preview.textureValid then
     return
   end
 
-  preview.orig_shader = love.graphics.getShader()
+  local viewport = viewportHandler.viewport
 
-  love.graphics.setShader(preview.pixShader)
+  if not col_preview.canvas
+     or (resize_timer <= 0 and
+         (col_preview.canvas:getWidth() ~= viewport.width or
+          col_preview.canvas:getWidth() ~= viewport.width)) then
+    resize_timer = 15
+    col_preview.canvas = love.graphics.newCanvas(viewport.width, viewport.height)
+  end
 
-  love.graphics.draw = preview._mod_draw
-  love.graphics.drawLayer = preview._mod_drawLayer
+  prev_canvas = love.graphics.getCanvas()
+  love.graphics.setCanvas(col_preview.canvas)
+  love.graphics.clear(0, 0, 0, 0)
+  love.graphics.push()
+  -- love.graphics.translate(math.floor(-viewport.x), math.floor(-viewport.y))
+  -- love.graphics.scale(viewport.scale, viewport.scale)
 end
 
-function preview.end_preview()
-  love.graphics.draw = preview._orig_draw
-  love.graphics.drawLayer = preview._orig_drawLayer
-
-  if preview.orig_shader then
-    love.graphics.setShader(preview.orig_shader)
+function col_preview.end_preview()
+  if not col_preview.enabled or not col_preview.textureValid then
+    return
   end
+
+  local viewport = viewportHandler.viewport
+
+  love.graphics.pop()
+  love.graphics.setCanvas(prev_canvas)
+  love.graphics.push()
+  -- love.graphics.translate(math.floor(viewport.x), math.floor(viewport.y))
+  -- love.graphics.scale(1 / viewport.scale, 1 / viewport.scale)
+  local prev_shader = love.graphics.getShader()
+  love.graphics.setShader(col_preview.shader)
+  love.graphics.setBlendMode("alpha", "premultiplied")
+  love.graphics.draw(col_preview.canvas, 0, 0)
+  love.graphics.setBlendMode("alpha", "alphamultiply")
+  love.graphics.setShader(prev_shader)
+  love.graphics.pop()
+end
+
+function col_preview.unload()
+  col_preview.currentColorgrade = nil
+  col_preview.textureValid = nil
+  col_preview.canvas = nil
+  resize_timer = 0
 end
 
 ---
 
-return preview
+return col_preview
