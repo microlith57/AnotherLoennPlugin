bf83acb168616703b90d0b0e731ac43d73d1692f
microlith57 <microlith57@gmail.com>
2023-03-30 21:54:38 +1300

    redo room move tool

--- Loenn/tools/room_mover.lua
+++ Loenn/tools/room_mover.lua
@@ -6,7 +6,7 @@ if not settings.featureEnabled("room_mover") then
 end
 
 local viewportHandler = require("viewport_handler")
-local loadedState = require("loaded_state")
+local state = require("loaded_state")
 local utils = require("utils")
 local history = require("history")
 local snapshot = require("structs.snapshot")
@@ -24,163 +24,166 @@ local tool = {
   }
 }
 
-local dragging
-local draggingStartX, draggingStartY
-local origX, origY = 0, 0
-local deltaX, deltaY = 0, 0
-local itemBeforeMove
-local tableItemsBeforeMove = {}
-local targetType
-local previousCursor
+local moving = {} -- list of { room, orig_x, orig_y }
+local startX, startY = 0, 0
+local currentX, currentY = 0, 0
 
 ---
 
-local function moveItem(item, orig_pos, itemType, dx, dy)
-  if itemType == "room" then
-    if not orig_pos.x or not orig_pos.y then return end
-    local new_x, new_y = orig_pos.x + 8 * math.floor(dx), orig_pos.y + 8 * math.floor(dy)
+local function moveRoom(room, orig_x, orig_y, dx, dy)
+  require("logging").info("moving "..room.name.." by "..dx..","..dy)
 
-    item.x = new_x
-    item.y = new_y
-  elseif itemType == "filler" then
-    if not orig_pos.x or not orig_pos.y then return end
-    local new_x, new_y = orig_pos.x + math.floor(dx), orig_pos.y + math.floor(dy)
+  room.x = math.floor((orig_x + dx) / 8) * 8
+  room.y = math.floor((orig_y + dy) / 8) * 8
 
-    item.x = new_x
-    item.y = new_y
-  elseif itemType == "table" then
-    for iitem, iitemType in pairs(item) do
-      local iorig_pos = tableItemsBeforeMove[iitem]
-      if iorig_pos and iorig_pos.x and iorig_pos.y and (iitemType == "room" or iitemType == "filler") then
-        moveItem(iitem, iorig_pos, iitemType, dx, dy)
-      end
-    end
-  end
+  return (room.x ~= orig_x) or (room.y ~= orig_y)
 end
 
 ---
 
-function tool.mousepressed(x, y, button, istouch, presses)
-  local item, itemType = loadedState.getSelectedItem()
+local function cancel(rooms)
+  local any = (#rooms > 0)
 
-  local canDrag = true
-  if itemType == "room" then
-    local cursorX, cursorY = viewportHandler.getRoomCoordinates(item, x, y)
-    if cursorX < 0 or cursorX > item.width or cursorY < 0 or cursorY > item.height then
-      canDrag = false
-    end
-  elseif itemType == "filler" then
-    local cursorX, cursorY = viewportHandler.getMapCoordinates(x, y)
-    if cursorX < item.x or cursorX > (item.x + item.width) * 8 or cursorY < item.y * 8 or cursorY > (item.y + item.height) * 8 then
-      canDrag = false
-    end
-  elseif itemType == "table" then
-    tableItemsBeforeMove = {}
-    for iitem, iitemtype in pairs(item) do
-      tableItemsBeforeMove[iitem] = {x = iitem.x, y = iitem.y}
-      if iitemType == "room" then
-        local cursorX, cursorY = viewportHandler.getRoomCoordinates(iitem, x, y)
-        if cursorX < 0 or cursorX > item.width or cursorY < 0 or cursorY > item.height then
-          canDrag = false
-        end
-      elseif iitemType == "filler" then
-        local cursorX, cursorY = viewportHandler.getMapCoordinates(x, y)
-        if cursorX < iitem.x or cursorX > (item.x + iitem.width) * 8 or cursorY < iitem.y * 8 or cursorY > (iitem.y + item.height) * 8 then
-          canDrag = false
-        end
-      end
-    end
+  for i, v in ipairs(rooms) do
+    local room, orig_x, orig_y = unpack(v)
+    room.x, room.y = orig_x, orig_y
+    rooms[i] = nil
   end
 
-  if canDrag then
-    dragging = true
-    madeChanges = false
-    deltaX, deltaY = 0, 0
-    if item.x and item.y then
-      origX, origY = item.x, item.y
-    end
-    itemBeforeMove = utils.deepcopy(item)
-    targetType = itemType
-  end
+  require("logging").info("cancelled move")
+  return any
 end
 
-function tool.mousereleased(x, y, button, istouch, presses)
-  local consume = not (not dragging)
+---
 
-  dragging = false
+function tool.mousepressed(x, y, _button, _istouch, _presses)
+  currentX, currentY = viewportHandler.getMapCoordinates(x, y)
 
-  if madeChanges then
-    local item, itemType = loadedState.getSelectedItem()
+  require("logging").info("mousepressed @ "..x..","..y)
 
-    local data = {
-      dx = deltaX,
-      dy = deltaY,
-      ox = itemBeforeMove.x,
-      oy = itemBeforeMove.y
-    }
-
-    local function backward(data)
-      moveItem(item, {x = data.ox, y = data.oy}, itemType, 0, 0)
-    end
-    local function forward(data)
-      moveItem(item, {x = data.ox, y = data.oy}, itemType, data.dx, data.dy)
-    end
-
-    history.addSnapshot(snapshot.create("Room move", data, backward, forward))
-    madeChanges = false
+  if #moving > 0 then
+    cancel(moving)
   end
 
-  return consume
-end
+  local sel, selType = state.getSelectedItem()
 
-function tool.mousemoved(x, y, dx, dy, istouch)
-  local item, itemType = loadedState.getSelectedItem()
+  if selType == "room" then
+    -- moving one room
 
-  if dragging then
-    if not item then
+    if currentX < sel.x or currentX >= sel.x + sel.width or
+       currentY < sel.y or currentY >= sel.y + sel.height then
       return false
     end
 
-    madeChanges = true
+    table.insert(moving, {sel, sel.x, sel.y})
+  elseif selType == "table" then
+    -- moving several things, hopefully all of which are rooms
+    local hovered = false
 
-    local cursorX, cursorY = viewportHandler.getMapCoordinates(x, y)
-    local tileX, tileY = cursorX / 8, cursorY / 8
+    for subsel, subselType in pairs(sel) do
+      if subselType == "room" then
+        if currentX >= subsel.x and currentX < subsel.x + subsel.width and
+           currentY >= subsel.y and currentY < subsel.y + subsel.height then
+          hovered = true
+        end
 
-    deltaX, deltaY = tileX - draggingStartX, tileY - draggingStartY
-    moveItem(item, {x = origX, y = origY}, itemType, deltaX, deltaY)
+        table.insert(moving, {subsel, subsel.x, subsel.y})
+      else
+        cancel(moving)
+        return false
+      end
+    end
 
+    if not hovered then
+      cancel(moving)
+      return false
+    end
+  else
+    return false
+  end
+
+  if #moving > 0 then
+    require("logging").info("began move")
+    startX, startY = currentX, currentY
     return true
   else
-    local cursorX, cursorY = viewportHandler.getMapCoordinates(x, y)
-    draggingStartX, draggingStartY = viewportHandler.pixelToTileCoordinates(cursorX, cursorY)
+    return false
   end
+end
+
+function tool.mousereleased(x, y, _button, _istouch, _presses)
+  currentX, currentY = viewportHandler.getMapCoordinates(x, y)
+  local dx, dy = currentX - startX, currentY - startY
+
+  require("logging").info("mousereleased @ "..x..","..y.."; δ="..dx..","..dy)
+
+  if #moving == 0 then
+    return false
+  end
+
+  local data = {
+    rooms = {}, -- map of { room.name = { orig_x, orig_y } }
+    dx = dx, dy = dy
+  }
+  for i, v in ipairs(moving) do
+    local room, orig_x, orig_y = unpack(v)
+    data.rooms[room.name] = {orig_x, orig_y}
+  end
+
+  local function backward(data)
+    for _, room in ipairs(state.map.rooms) do
+      if data.rooms[room.name] then
+        room.x, room.y = unpack(data.rooms[room.name])
+      end
+    end
+  end
+
+  local function forward(data)
+    local dx, dy = data.dx, data.dy
+
+    for _, room in ipairs(state.map.rooms) do
+      if data.rooms[room.name] then
+        local orig_x, orig_y = unpack(data.rooms[room.name])
+        moveRoom(room, orig_x, orig_y, dx, dy)
+      end
+    end
+  end
+
+  local modified = false
+
+  for i, v in ipairs(moving) do
+    local room, orig_x, orig_y = unpack(v)
+    modified = moveRoom(room, orig_x, orig_y, dx, dy) or modified
+    moving[i] = nil
+  end
+
+  if not modified then return false end
+
+  require("logging").info("committed move")
+  history.addSnapshot(snapshot.create("Room move", data, backward, forward))
+  return true
+end
+
+function tool.mousemoved(x, y, _dx, _dy, _istouch)
+  currentX, currentY = viewportHandler.getMapCoordinates(x, y)
+  local dx, dy = currentX - startX, currentY - startY
+
+  if #moving == 0 then
+    return false
+  end
+
+  for i, v in pairs(moving) do
+    local room, orig_x, orig_y = unpack(v)
+    moveRoom(room, orig_x, orig_y, dx, dy)
+  end
+
+  return true
 end
 
 function tool.editorMapTargetChanged()
-  dragging = false
-
-  if madeChanges then
-    local item, itemType = loadedState.getSelectedItem()
-
-    local data = {
-      dx = deltaX,
-      dy = deltaY,
-      ox = itemBeforeMove.x,
-      oy = itemBeforeMove.y
-    }
-
-    local function backward(data)
-      moveItem(item, {x = data.ox, y = data.oy}, itemType, 0, 0)
-    end
-    local function forward(data)
-      moveItem(item, {x = data.ox, y = data.oy}, itemType, data.dx, data.dy)
-    end
-
-    history.addSnapshot(snapshot.create("Room move", data, backward, forward))
-    madeChanges = false
-  end
+  -- tool.mousereleased(currentX, currentY)
+  cancel(moving)
 end
-
 
 ---
 
