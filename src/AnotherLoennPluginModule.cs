1e6a22472c6a289921451ea64fe7461e169809ed
microlith57 <microlith57@gmail.com>
2023-04-14 17:31:51 +1200

    template

---
+++ src/AnotherLoennPluginModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Celeste.Mod.AnotherLoennPlugin {
+    public class AnotherLoennPluginModule : EverestModule {
+        public static AnotherLoennPluginModule Instance { get; private set; }
+
+        public override Type SettingsType => typeof(AnotherLoennPluginModuleSettings);
+        public static AnotherLoennPluginModuleSettings Settings => (AnotherLoennPluginModuleSettings) Instance._Settings;
+
+        public AnotherLoennPluginModule() {
+            Instance = this;
+#if DEBUG
+            // debug builds use verbose logging
+            Logger.SetLogLevel(nameof(AnotherLoennPluginModule), LogLevel.Verbose);
+#else
+            // release builds use info logging to reduce spam in log files
+            Logger.SetLogLevel(nameof(AnotherLoennPluginModule), LogLevel.Info);
+#endif
+        }
+
+        public override void Load() {
+            // TODO: apply any hooks that should always be active
+        }
+
+        public override void Unload() {
+            // TODO: unapply any hooks applied in Load()
+        }
+    }
+}
---
+++ src/AnotherLoennPluginModuleSettings.cs
@@ -0,0 +1,5 @@
+namespace Celeste.Mod.AnotherLoennPlugin {
+    public class AnotherLoennPluginModuleSettings : EverestModuleSettings {
+
+    }
+}
