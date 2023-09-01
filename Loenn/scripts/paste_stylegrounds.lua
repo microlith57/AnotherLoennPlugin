78e78d8d8501f1b6c11494606c26d174b851ad4f
microlith57 <microlith57@gmail.com>
2023-09-02 09:52:42 +1200

    add more options to paste stylegrounds script

--- Loenn/scripts/paste_stylegrounds.lua
+++ Loenn/scripts/paste_stylegrounds.lua
@@ -5,14 +5,37 @@ local snapshot = require("structs.snapshot")
 local script = {
   name = "pasteStylegroundsFromClipboard",
   parameters = {
-    replaceExistingStylegrounds = false
+    replaceExistingStylegrounds = false,
+    replaceOnly = "",
+    addTags = ""
   },
   displayName = "Clipboard: Paste Stylegrounds",
   tooltip = "Pastes stylegrounds from the clipboard into the map.",
   tooltips = {
-    replaceExistingStylegrounds = "Whether to replace the existing stylegrounds."
+    replaceExistingStylegrounds = "Whether to replace the existing stylegrounds.",
+    replaceOnly = "If not blank, replace the Only fields with this.",
+    addTags = "Add these tags to all pasted stylegrounds."
   }
 }
+
+local function forward_replace(data)
+  state.map.stylesFg = data.newFg
+  state.map.stylesBg = data.newBg
+end
+
+local function forward_noreplace(data)
+  for i, style in ipairs(data.newFg) do
+    table.insert(state.map.stylesFg, style)
+  end
+  for i, style in ipairs(data.newBg) do
+    table.insert(state.map.stylesBg, style)
+  end
+end
+
+local function backward(data)
+  state.map.stylesFg = data.oldFg
+  state.map.stylesBg = data.oldBg
+end
 
 function script.prerun(args)
   local oldStylesFg = utils.deepcopy(state.map.stylesFg)
@@ -26,34 +49,43 @@ function script.prerun(args)
   local success, fromClipboard = utils.unserialize(clipboard, true, 3)
   if not success then return false end
 
-  -- todo move forward, backward outside of prerun, and use data
+  local data = {
+    oldFg = utils.deepcopy(state.map.stylesFg),
+    oldBg = utils.deepcopy(state.map.stylesBg),
+    newFg = {},
+    newBg = {}
+  }
 
-  local function forward(data)
-    state.map.stylesFg = fromClipboard.fg
-    state.map.stylesBg = fromClipboard.bg
-  end
-
-  local function forward_noreplace(data)
-    for i, style in ipairs(fromClipboard.fg) do
-      table.insert(state.map.stylesFg, style)
+  for i, style in ipairs(fromClipboard.fg or {}) do
+    if args.replaceOnly ~= "" then
+      style.only = args.replaceOnly
     end
-    for i, style in ipairs(fromClipboard.bg) do
-      table.insert(state.map.stylesBg, style)
+    if args.addTags ~= "" then
+      if style.tag ~= "" then
+        style.tag = style.tag + ','
+      end
+      style.tag = style.tag + args.addTags
     end
+    table.insert(data.newFg, style)
   end
 
-  local function backward(data)
-    state.map.stylesFg = oldStylesFg
-    state.map.stylesBg = oldStylesBg
+  for i, style in ipairs(fromClipboard.bg or {}) do
+    if args.replaceOnly ~= "" then
+      style.only = args.replaceOnly
+    end
+    if args.addTags ~= "" then
+      if style.tag ~= "" then
+        style.tag = style.tag .. ','
+      end
+      style.tag = style.tag .. args.addTags
+    end
+    table.insert(data.newBg, style)
   end
 
-  if args.replaceExistingStylegrounds then
-    forward()
-    return snapshot.create(script.name, {}, backward, forward)
-  else
-    forward_noreplace()
-    return snapshot.create(script.name, {}, backward, forward_noreplace)
-  end
+  local forward = args.replaceExistingStylegrounds and forward_replace or forward_noreplace
+  forward(data)
+
+  return snapshot.create(script.name, data, backward, forward)
 end
 
 return script
